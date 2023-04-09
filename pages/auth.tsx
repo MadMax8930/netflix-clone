09dc572e4bed198e5222267787cd2da479801d99
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-04-09 04:13:32 +0200

    login ui

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,4 +1,11 @@
+import Input from "@/components/Input";
+import { useState } from "react";
+
 const Auth = () => {
+   const [email, setEmail] = useState('');
+   const [name, setName] = useState('');
+   const [password, setPassword] = useState('');
+
    return (
       <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
          <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -10,6 +17,37 @@ const Auth = () => {
                   <h2 className="text-white text-4xl mb-8 font-semibold">
                      Sign in
                   </h2>
+                  <div className="flex flex-col gap-4">
+                     <Input 
+                        label="Username"
+                        id="name" 
+                        onChange={(e: any) => setName(e.target.value)}
+                        value={name}
+                     />
+                     <Input 
+                        label="Email"
+                        id="email" 
+                        onChange={(e: any) => setEmail(e.target.value)}
+                        type="email"
+                        value={email}
+                     />
+                     <Input 
+                        label="Password"
+                        id="password" 
+                        onChange={(e: any) => setPassword(e.target.value)}
+                        type="password"
+                        value={password}
+                     />
+                  </div>
+                  <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                     Login
+                  </button>
+                  <p className="text-neutral-500 mt-12">
+                     First time using Netflix?
+                     <span className="text-white ml-1 hover:underline cursor-pointer">
+                        Create an account
+                     </span>
+                  </p>
                </div>
             </div>
          </div>
